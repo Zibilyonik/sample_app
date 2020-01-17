9230ec247e5c10ef450774536b96e70ee6323579
Zibilyonik <antagonistpotato@gmail.com>
2020-01-17 19:28:46 +0000

    SSl and Puma use in webserver in production added

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,8 +11,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
-    assert_select 'div#<CSS id for error explanation>'
-    assert_select 'div.<CSS class for field with error>'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
   
   test "valid signup information" do
