4b117503dbdd54230143e9a568a9d562f6e29077
Zibilyonik <antagonistpotato@gmail.com>
2020-01-20 21:56:58 +0000

    added basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -25,6 +25,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.nil?
+    assert is_logged_in?
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,12 +5,10 @@ require "minitest/reporters"
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
-
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-  include ApplicationHelper
 
-  # Add more helper methods to be used by all tests here...
+  # Returns true if a test user is logged in.
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
